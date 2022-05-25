5d62d423cead014caa64c5606fb08a575ce4ad61
Jacob Litel-Smith <litelsmithj@gmail.com>
2022-05-25 07:29:27 -0700

    Added frontend auth and redux

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,13 +1,37 @@
 import { useState, useEffect } from "react";
 import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
-function Register() {
+function Login() {
   const [formData, setFormData] = useState({
     email: "",
     password: ""
   });
 
   const { email, password } = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isSuccess, isError, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -18,7 +42,18 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password
+    };
+
+    dispatch(login(userData));
   };
+
+  if (isLoading) {
+    return <Spinner/>
+  }
 
   return (
     <>
@@ -64,4 +99,4 @@ function Register() {
   );
 }
 
-export default Register;
+export default Login;
